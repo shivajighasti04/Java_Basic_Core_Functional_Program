0af4ef72f5b564744153a55f80935e6e2ba7dc04
shivajighasti04 <shivajighasti97@gamil.com>
2022-06-23 20:47:53 +0530

    Added : Java Program to Check Whether a Number is Even or Odd

---
+++ src/com/bridgelabz/coreprograms/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.coreprograms;
+
+import Utility.Util;
+
+/**
+ *  Java Program to Check Whether a Number is Even or Odd
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class EvenOrOdd {
+		public static void main(String[] args) {
+			Util.checkEvenOdd();
+		}
+}
