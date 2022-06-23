019698ac79237bed3405692fa88a339064b2112b
shivajighasti04 <shivajighasti97@gamil.com>
2022-06-23 20:42:11 +0530

    Added : Program to Compute Quotient and Remainder

---
+++ src/com/bridgelabz/coreprograms/QuotientRemainder.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.coreprograms;
+
+import java.util.Scanner;
+
+import Utility.Util;
+
+/**
+ *  Java Program to Compute Quotient and Remainder
+ * 
+ * @author Shivaji Ghasti
+ *
+ */
+public class QuotientRemainder {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter dividend number: ");
+			int dividend = sc.nextInt();
+			System.out.print("Enter divisior number: ");
+			int divisor = sc.nextInt();
+
+			int quotient = Util.quotient(dividend, divisor);
+			int remainder = Util.remainder(dividend, divisor);
+
+			System.out.println("Quotient = " + quotient);
+			System.out.println("Remainder = " + remainder);
+		}
+
+}
